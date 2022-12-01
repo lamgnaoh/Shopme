5a2b4e1685055d159e0b8578493ca57e2fe0f6d3
lamgnaoh <luonglamhoang2000@gmail.com>
2022-12-02 01:35:07 +0700

    code category entity and category repository

---
+++ ShopmeWebParent/ShopmeBackend/src/test/java/com/shopme/admin/category/CategoryRepositoryTests.java
@@ -0,0 +1,56 @@
+package com.shopme.admin.category;
+
+import com.shopme.common.entity.Category;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
+
+import java.util.List;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest(showSql = false)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE) // run test with the real database not in-memory database
+@Rollback(false) // keep the data in database after the test
+public class CategoryRepositoryTests {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Test
+    public void testCreateRootCategory() {
+        Category category = new Category("Electronics");
+        Category savedCategory = categoryRepository.save(category);
+
+        assertThat(savedCategory.getId()).isGreaterThan(0);
+    }
+
+    @Test
+    public void testCreateSubCategory() {
+        Category parent = new Category(7);
+        Category memory = new Category("Iphones", parent);
+        Category savedCategory = categoryRepository.save(memory);
+        assertThat(savedCategory.getId()).isGreaterThan(0);
+
+
+    }
+
+    @Test
+    public void testGetCategory() {
+        Category category = categoryRepository.findById(2).get();
+        System.out.println(category.getName());
+
+        Set<Category> children = category.getChildren();
+
+        for (Category subCategory : children) {
+            System.out.println(subCategory.getName());
+        }
+
+        assertThat(children.size()).isGreaterThan(0);
+    }
+
+}
+
