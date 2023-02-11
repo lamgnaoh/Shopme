0e317e7b303644e397f9ea71523a8e5a56685b02
lamgnaoh <luonglamhoang2000@gmail.com>
2023-02-11 21:52:45 +0700

    code shopme application list product and categories in homepage

--- ShopmeWebParent/ShopmeFrontend/src/main/java/com/shopme/MainController.java
+++ ShopmeWebParent/ShopmeFrontend/src/main/java/com/shopme/MainController.java
@@ -1,12 +1,24 @@
 package com.shopme;
 
+import com.shopme.common.entity.Category;
+import com.shopme.category.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class MainController {
+    @Autowired
+    private CategoryService categoryService;
+
     @GetMapping("")
-    public String viewHomepage(){
+    public String viewHomePage(Model model) {
+        List<Category> listCategories = categoryService.listNoChildrenCategories();
+        model.addAttribute("listCategories", listCategories);
+
         return "index";
     }
 
---
+++ ShopmeWebParent/ShopmeFrontend/src/main/java/com/shopme/product/ProductRepository.java
@@ -0,0 +1,18 @@
+package com.shopme.product;
+
+import com.shopme.common.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface ProductRepository extends PagingAndSortingRepository<Product, Integer> {
+
+//	list product by category id or in sub category of the selected category
+	@Query("SELECT p FROM Product p WHERE p.enabled = true "
+			+ "AND (p.category.id = :categoryId OR p.category.allParentIDs LIKE %:categoryIdMatch%)"
+			+ " ORDER BY p.name ASC")
+	Page<Product> listByCategory(Integer categoryId, String categoryIdMatch, Pageable pageable);
+
+
+}
