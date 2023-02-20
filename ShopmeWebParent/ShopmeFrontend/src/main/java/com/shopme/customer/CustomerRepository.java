02e3b73bb0831328e11c723db4052d3b622175df
lamgnaoh <luonglamhoang2000@gmail.com>
2023-02-21 01:55:19 +0700

    code customer login by google account

--- ShopmeWebParent/ShopmeFrontend/src/main/java/com/shopme/customer/CustomerRepository.java
+++ ShopmeWebParent/ShopmeFrontend/src/main/java/com/shopme/customer/CustomerRepository.java
@@ -1,5 +1,6 @@
 package com.shopme.customer;
 
+import com.shopme.common.entity.AuthenticationType;
 import com.shopme.common.entity.Customer;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -18,4 +19,8 @@ public interface CustomerRepository extends CrudRepository<Customer, Integer> {
 	@Query("UPDATE Customer c SET c.enabled = true , c.verificationCode = null  WHERE c.id = :id")
 	@Modifying
 	 void enable(Integer id);
+
+	@Query("UPDATE Customer c SET c.authenticationType = :type WHERE c.id = :customerId")
+	@Modifying
+	void updateAuthenticationType(Integer customerId, AuthenticationType type);
 }
--- ShopmeWebParent/ShopmeFrontend/src/main/java/com/shopme/customer/CustomerService.java
+++ ShopmeWebParent/ShopmeFrontend/src/main/java/com/shopme/customer/CustomerService.java
@@ -1,5 +1,6 @@
 package com.shopme.customer;
 
+import com.shopme.common.entity.AuthenticationType;
 import com.shopme.common.entity.Country;
 import com.shopme.common.entity.Customer;
 import com.shopme.setting.CountryRepository;
@@ -42,6 +43,7 @@ public class CustomerService {
 		encodePassword(customer);
 		customer.setEnabled(false);
 		customer.setCreatedTime(new Date());
+		customer.setAuthenticationType(AuthenticationType.DATABASE);
 
 		String randomCode = RandomString.make(64);
 		customer.setVerificationCode(randomCode);
@@ -60,5 +62,49 @@ public class CustomerService {
 			return true;
 		}
 	}
+
+	public void updateAuthenticationType(Customer customer, AuthenticationType type) {
+		if (!customer.getAuthenticationType().equals(type)) {
+			customerRepo.updateAuthenticationType(customer.getId(), type);
+		}
+	}
+
+	public Customer getCustomerByEmail(String email) {
+		return customerRepo.findByEmail(email);
+	}
+
+	public void addNewCustomerUponOAuthLogin(String name, String email, String countryCode) {
+		Customer customer = new Customer();
+		customer.setEmail(email);
+		setName(name, customer);
+
+		customer.setEnabled(true);
+		customer.setCreatedTime(new Date());
+		customer.setAuthenticationType(AuthenticationType.GOOGLE);
+		customer.setPassword("");
+		customer.setAddressLine1("");
+		customer.setCity("");
+		customer.setState("");
+		customer.setPhoneNumber("");
+		customer.setPostalCode("");
+		customer.setCountry(countryRepo.findByCode(countryCode));
+
+		customerRepo.save(customer);
+	}
+
+	private void setName(String name, Customer customer) {
+		String[] nameArray = name.split(" ");
+		if (nameArray.length < 2) {
+			customer.setFirstName(name);
+			customer.setLastName("");
+		} else {
+			String firstName = nameArray[0];
+			customer.setFirstName(firstName);
+
+			String lastName = name.replaceFirst(firstName, "");
+			customer.setLastName(lastName);
+		}
+	}
+
 
 }
